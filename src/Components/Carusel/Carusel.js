77a6943e195e5b6d3bd04af217d0420f8c8583bd
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2021-07-18 21:22:14 -0500

    Agregado Carousel faltando modificar estilos

--- src/Components/Carusel/Carusel.js
+++ src/Components/Carusel/Carusel.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import './Carusel.css'
+import Carousel from "react-elastic-carousel";
+import CaruselItem from './CaruselItem/CaruselItem.js';
 import websiteIcon from "./websiteIcon.png";
 import websiteIcon1 from "./websiteIcon1.png";
 import websiteIcon2 from "./websiteIcon2.png";
 import websiteIcon3 from "./websiteIcon3.png";
+import websiteIcon4 from "./websiteIcon4.png";
+import websiteIcon5 from "./websiteIcon5.png";
+
+const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
 
 export default function Carusel() {
-    return (
-    <div className="o-carusel-container">
-        <div className="o-carusel-item-container">
-            <img src={websiteIcon} alt= "Website Icon" className="o-carusel-item-img"></img>
-            <p className="o-carusel-item-text">Flexibilidad Horaria</p>
-        </div>
-        <div className="o-carusel-item-container">
-            <img src={websiteIcon1} alt= "Website Icon" className="o-carusel-item-img"></img>
-            <p className="o-carusel-item-text">Home office</p>
-        </div>
-        <div className="o-carusel-item-container">
-            <img src={websiteIcon2} alt= "Website Icon" className="o-carusel-item-img"></img>
-            <p className="o-carusel-item-text">Capacitaciones y workshops</p>
-        </div>
-        <div className="o-carusel-item-container">
-            <img src={websiteIcon3} alt= "Website Icon" className="o-carusel-item-img"></img>
-            <p className="o-carusel-item-text">Snacks, frutas y bebidas gratis</p>
-        </div>
-    </div>
+    return (  
+        <Carousel breakPoints={breakPoints} className="o-carusel-container">
+            <CaruselItem icon={websiteIcon} text="Flexibilidad Horaria"></CaruselItem>
+            <CaruselItem icon={websiteIcon1} text="Home office"></CaruselItem>
+            <CaruselItem icon={websiteIcon2} text="Capacitaciones y workshops"></CaruselItem>
+            <CaruselItem icon={websiteIcon3} text="Snacks, frutas y bebidas gratis"></CaruselItem>
+            <CaruselItem icon={websiteIcon4} text="Semana Remota"></CaruselItem>
+            <CaruselItem icon={websiteIcon5} text="Trabajar en ultimas tecnologias"></CaruselItem>
+        </Carousel>
     )
 }
--- src/Components/Carusel/CaruselItem/CaruselItem.js
+++ src/Components/Carusel/CaruselItem/CaruselItem.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-export default function CaruselItem() {
+export default function CaruselItem(props) {
     return (
         <div className="o-carusel-item-container">
-            <img src={websiteIcon} alt= "Website Icon" className="o-carusel-item-img"></img>
-            <p className="o-carusel-item-text">Flexibilidad Horaria</p>
+            <img src={props.icon} alt= "Website Icon" className="o-carusel-item-img"></img>
+            <p className="o-carusel-item-text">{props.text}</p>
         </div>
     )
 }
