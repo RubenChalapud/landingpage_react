1f3c20ca75b8f76b81d8107a7454e2363b4287ce
Ruben Chalapud <ruben.chalapud@uao.edu.co>
2021-07-18 20:34:48 -0500

    Organizada estructura de Carusel

---
+++ src/Components/Carusel/CaruselItem/CaruselItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CaruselItem() {
+    return (
+        <div className="o-carusel-item-container">
+            <img src={websiteIcon} alt= "Website Icon" className="o-carusel-item-img"></img>
+            <p className="o-carusel-item-text">Flexibilidad Horaria</p>
+        </div>
+    )
+}
